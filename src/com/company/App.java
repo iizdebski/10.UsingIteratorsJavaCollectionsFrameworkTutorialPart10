6370a0a0d25f3adddb673b8dd1586cd004368c81
SwAppCoder <iizdepskyy112@gmail.com>
2020-04-02 14:44:14 +0300

    Using Iterators: Java Collections Framework Tutorial Part 10

---
+++ src/com/company/App.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> animals = new LinkedList<String>();
+
+        animals.add("fox");
+        animals.add("cat");
+        animals.add("dog");
+        animals.add("rabbit");
+
+        Iterator<String> it = animals.iterator();
+
+        while (it.hasNext()) {
+            String value = it.next();
+            System.out.println(value);
+
+            if (value.equals("cat")) {
+                it.remove();
+            }
+        }
+
+        System.out.println();
+
+        // Iteration
+
+        for (String animal: animals) {
+            System.out.println(animal);
+
+            // animals.remove(2);
+        }
+    }
+}
